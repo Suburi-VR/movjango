1425e6251b367b59a53b254739c760eb36c248e7
Suburi-VR <soburi.wo.suru.suburi@gmail.com>
2020-11-16 21:58:55 +0900

    updated

--- moviedjango/views.py
+++ moviedjango/views.py
@@ -15,7 +15,6 @@ from django.contrib import messages
 
 
 
-@login_required(login_url='/accounts/login/')
 def toppage(request):
     if request.method == 'GET':
         page_num = request.GET.get('p', 1)
