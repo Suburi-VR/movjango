265fdc872b4aeab8b3cef36ba213f505d6575418
Suburi-VR <soburi.wo.suru.suburi@gmail.com>
2020-11-16 22:48:40 +0900

    upadted

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 
@@ -141,7 +141,7 @@ if hostname == 'NatitanoMacBook-Pro.local':
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'db.sqlite3',
         }
     }
 else:
