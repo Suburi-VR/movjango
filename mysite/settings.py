77cb135919c160c403579f13d5192c9d7950386c
Suburi-VR <soburi.wo.suru.suburi@gmail.com>
2020-11-16 22:00:28 +0900

    updated

--- mysite/settings.py
+++ mysite/settings.py
@@ -147,7 +147,7 @@ if hostname == 'NatitanoMacBook-Pro.local':
     }
 else:
     print('fugafuga')
-    DEBUG = False
+    DEBUG = True
     ALLOWED_HOSTS = ['movdjango.herokuapp.com']
     import dj_database_url
     db_from_env = dj_database_url.config()
