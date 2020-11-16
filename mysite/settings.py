38e4c6bab66755e1602ffecb6c283704e34253e3
Suburi-VR <soburi.wo.suru.suburi@gmail.com>
2020-11-16 22:51:41 +0900

    upadted

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+print(STATIC_ROOT)
 
 IMAGE_ROOT = os.path.join(BASE_DIR, 'images')
 IMAGE_URL = '/images/'
